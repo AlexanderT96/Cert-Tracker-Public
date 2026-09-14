8646428bc70951056ee6622bbca80a5b2d71b7f9
AlexanderT96 <172367340+AlexanderT96@users.noreply.github.com>
2026-09-14 22:32:21 +0100

    Complete Template One visual convergence

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
   const CT = global.CertTrackerV3 = global.CertTrackerV3 || {};
 
   CT.version = Object.freeze({
-    app: '4.26.7',
+    app: '4.27.0',
     data: 71,
     storage: 12,
     backup: 11,
